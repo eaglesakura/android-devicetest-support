f2f6dcc789aa9904cac1c58847ebfe36bbc5eefd
eaglesakura <eagle.sakura@gmail.com>
2016-03-09 11:10:49 +0900

    Utilを追加

---
+++ src/main/java/com/eaglesakura/android/devicetest/TestUtils.java
@@ -0,0 +1,38 @@
+package com.eaglesakura.android.devicetest;
+
+import com.eaglesakura.android.device.external.StorageInfo;
+import com.eaglesakura.util.IOUtil;
+import com.eaglesakura.util.LogUtil;
+
+import android.content.Context;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestUtils {
+
+    private static final Date gInitDate = new Date();
+
+    private static File gCacheRoot;
+
+    private static File gCacheDirectory;
+
+    public static synchronized File getCacheDirectory(Context context) {
+        if (gCacheRoot == null) {
+            gCacheRoot = new File(StorageInfo.getExternalStorageRoot(context), "junit");
+        }
+
+        if (gCacheDirectory == null) {
+            SimpleDateFormat DEFAULT_FORMATTER = new SimpleDateFormat("yyyy-MM-dd-HHmmssSSSS");
+            gCacheDirectory =
+                    new File(gCacheRoot,
+                            String.format("%s", DEFAULT_FORMATTER.format(gInitDate))
+                    );
+            IOUtil.mkdirs(gCacheDirectory);
+            LogUtil.log("Cache Directory -> %s", gCacheDirectory.getAbsolutePath());
+        }
+
+        return gCacheDirectory;
+    }
+}
