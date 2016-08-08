a6a6ea8b0e62b3e2b05acc3c8d3a8b8503d5fdef
eaglesakura <eagle.sakura@gmail.com>
2016-08-08 09:00:07 +0900

    Activity/Fragment用のValidatorを追加

---
+++ src/main/java/com/eaglesakura/android/devicetest/validator/FragmentValidator.java
@@ -0,0 +1,52 @@
+package com.eaglesakura.android.devicetest.validator;
+
+import com.eaglesakura.lambda.Action1;
+
+import org.junit.Assert;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Fragment関連のvalidateを行う
+ */
+public class FragmentValidator extends BaseUiValidator<Fragment> {
+    ActivityValidator mParent;
+
+    Fragment mFragment;
+
+    public FragmentValidator(ActivityValidator parent, Fragment fragment) {
+        mParent = parent;
+        mFragment = fragment;
+    }
+
+    @Override
+    protected FragmentManager getFragmentManager() {
+        return mFragment.getChildFragmentManager();
+    }
+
+    @Override
+    protected ActivityValidator getActivityValidator() {
+        return mParent;
+    }
+
+    @Override
+    protected Fragment getTarget() {
+        return mFragment;
+    }
+
+    public <FragmentClass extends Fragment> FragmentValidator check(Class<FragmentClass> clazz, Action1<FragmentClass> action) {
+        try {
+            action.action((FragmentClass) mFragment);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+        return this;
+    }
+
+    public ActivityValidator doneFragment() {
+        return mParent;
+    }
+
+}
